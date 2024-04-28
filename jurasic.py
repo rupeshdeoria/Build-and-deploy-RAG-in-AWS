e46b75a8329c15f7e5ae590fe23551587ca407f5
rupeshdeoria <rupeshdeoria@gmail.com>
2024-04-28 10:11:00 +0530

    access stable diffusion model on bedrock

---
+++ jurasic.py
@@ -0,0 +1,11 @@
+import os
+import json
+import sys
+import boto3
+
+prompt="""
+
+        you are a cricket expert now just tell me when RCB will win the IPL?
+"""
+
+bedrock=boto3.client(service_name="bedrock-runtime")
